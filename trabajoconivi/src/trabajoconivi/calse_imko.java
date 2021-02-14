908c8f2e1b859dc57066d91c9f5de34e391d0886
2000n <2000n@DESKTOP-VPV62E2>
2021-02-14 18:26:17 +0100

    eeeee

--- trabajoconivi/src/trabajoconivi/calse_imko.java
+++ trabajoconivi/src/trabajoconivi/calse_imko.java
@@ -1,5 +1,41 @@
 package trabajoconivi;
 
-public class calse_imko {
 
-}
+
+	//package java_el_hut;
+	import java.util.Iterator;
+	import java.util.Scanner;
+
+	//@nicoalssomoza ejer140
+	public class calse_imko {
+
+		public static void main(String[] args) {
+			Scanner sr = new Scanner(System.in);
+			String numumero = sr.nextLine();
+			for (int array = 1; Integer.parseInt(numumero) >= 0;) {
+				String cadena[] = numumero.split("");
+				array = 0;
+				for (String elto : cadena) {
+					array += Integer.parseInt(elto);
+				}
+				int i;
+				for (i = 0; i < cadena.length; i++) {
+					System.out.print(cadena[i]);
+					if (i == cadena.length - 1) {
+						System.out.print(" ");
+					} else {
+						System.out.print(" + ");
+					}				
+				}
+
+				System.out.print(" = ");
+				System.out.println(array);
+				numumero = sr.nextLine();
+			}
+			sr.close();
+		}
+		
+	}
+
+
+
